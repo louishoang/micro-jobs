6b142ec56113eae03155e3f83133e318e90a9db9
louis hoang <bawsing@gmail.com>
2014-10-08 17:55:33 -0400

    finished test for user edit profile

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,6 +7,23 @@ feature "User login using facebook" do
 
     visit root_path
     click_on "Sign in"
-    save_and_open_page
+
+    expect(page).to have_content("Successfully signed in")
+  end
+
+  scenario "user can edit profile" do
+    user = FactoryGirl.create(:user)
+    mock_auth_hash_for(user)
+
+    visit root_path
+    click_on "Sign in"
+
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
+    fill_in "Address", with: user.address
+
+    click_on "Update User"
+
+    expect(page).to have_content("User profile is updated successfully")
   end
 end
