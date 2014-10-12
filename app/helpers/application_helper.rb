942ba2d6b00721fafc17c253a296633dd7ed7ea2
louis hoang <bawsing@gmail.com>
2014-10-12 00:41:28 -0400

    added comment to map.js to remind myself to fix DRY added js to make sortable table which display all jobs of a user in dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
+
+    # add css class to make arrow up and down
     css_class = column == sort_column ? "current #{sort_direction}" : nil
+    #direction of sort
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {sort: column, direction: direction}, {class: css_class}
   end
